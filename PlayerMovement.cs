e976b2dd1256ea8903114fdcc67a19bc8b42e03d
Pierce Connaughton <partypierce1234@gmail.com>
2023-02-06 01:09:49 +0000

    Able to move the player left and right

---
+++ PlayerMovement.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    //necssary for animations and physics
+    private Rigidbody2D rb2D;
+    private Animator myAnimator;
+
+    //variables to play with
+    public float speed = 2.0f;
+    public float horizMovement; // =1 or -1 or 0
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        //define the gameobjects found on the player
+        rb2D = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    //Handles input for physics
+    private void Update()
+    {
+        //check direction given by player   
+        horizMovement = Input.GetAxisRaw("Horizontal");
+    }
+
+    //handles running the physics
+    private void FixedUpdate()
+    {
+        //move the character left or right
+        rb2D.velocity = new Vector2(horizMovement * speed, rb2D.velocity.y);
+    }
+}
